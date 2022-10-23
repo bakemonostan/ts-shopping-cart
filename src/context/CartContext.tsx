4a58bc85d68e9353ab344677b74a57fb7672f6b3
bakemonostan <ehizomokhomion@gmail.com>
2022-10-23 15:53:24 -0700

    local storage

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { ShoppingCart } from '../components/ShoppingCart';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 // define types
 type CartProviderProps = {
@@ -31,7 +32,10 @@ export function useShoppingCart() {
 // export cart provider
 
 export function CartProvider({ children }: CartProviderProps) {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    'shopping-cart',
+    []
+  );
   const [isOpen, setIsOpen] = useState(false);
 
   // get item quantity
